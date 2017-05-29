3a793197d9c7d321a2ea890dec59ab353331303e
vladbusov <to2002to2002@gmail.com>
2017-05-29 03:59:18 +0300

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,12 @@
+#include "databasecore.h"
+#ifndef MENU
+void start(table *t);
+void pushup();
+void insbook(table *t);
+void delbook(table *t);
+void gbook(table *t);
+void pbook(table *t);
+void leave(table *t);
+void back(table *t);
+void menu(table *t);
+#endif
